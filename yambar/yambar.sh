193117a56cdbc611e482ab216c3c899b625f468a
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-07-10 09:23:51 +0800

    update colors to opensuse's palette guidelines

--- yambar/yambar.sh
+++ yambar/yambar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-killall -q $HOME/.local/bin/yambar
+killall -q /usr/bin/yambar
 while pgrep -x yambar >/dev/null; do sleep 1; done
 #exec yambar --config $HOME/.config/yambar/bottom.yml & disown
-exec $HOME/.local/bin/yambar & disown
+exec yambar & disown
